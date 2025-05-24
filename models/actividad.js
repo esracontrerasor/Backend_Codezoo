5f8a7d2823cfebff673fb1ccdfd8b6ecc36e7efb
Alan Cuevas <162539480+MrMetalXD@users.noreply.github.com>
2025-05-23 22:23:02 -0700

    cambios en el modelo de actividad
    cambios en el modelo de actividad

--- models/actividad.js
+++ models/actividad.js
@@ -3,7 +3,9 @@ import * as mongoose from 'mongoose';
 const actividadSchema = new mongoose.Schema({
     titulo : { type: String, required: true },
     descripcion : { type: String, required: true },
-    tipo: { type: String, required: true },
+    tipo: { type: String, enum: ["Juego","Cuento","Modo_Libre"],required: true },
+    image: { type: String, required: true},
+    ruta: { type: String, required: true},
     fechaCreacion : { type: Date, required: true },
     fechaActualizacion : { type: Date, required: true },
 });
